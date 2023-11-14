21c0cdfc9721e1662d8d37200e637c676f578099
William Yi <yiwilliam1098@gmail.com>
2023-11-14 18:56:32 -0500

    Configure firebase fetch for mock profiles

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import Home from "./components/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
 import { Profiles } from "./components/Profiles/Profiles";
 import { Layout } from "./components/Layout/Layout";
 import { InfoPage } from "./components/InfoPage";
+import CreateProfile from "./components/Profiles/CreateProfile";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/" Component={Home} />
             <Route path="/profiles" Component={Profiles} />
             <Route path="/profiles/:id" Component={InfoPage} />
+            <Route path="/profiles/create-profile" Component={CreateProfile} />
           </Routes>
         </BrowserRouter>
       </Layout>
--- client/src/components/Profiles/Profiles.js
+++ client/src/components/Profiles/Profiles.js
@@ -1,32 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Avatar } from "@mui/material";
 import { lightBlue } from "@mui/material/colors";
-import { mockProfiles } from "../../data/mockProfiles";
 import "./Profiles.css";
 
 export const Profiles = () => {
-  const [profiles, setProfiles] = useState(mockProfiles);
+  const [profiles, setProfiles] = useState({});
+
+  useEffect(() => {
+    fetch("/profiles")
+      .then((res) => res.json())
+      .then((data) => {
+        setProfiles(data);
+        console.log(data);
+      });
+  }, []);
 
   return (
     <div className="container">
       <h2>Select User Profile</h2>
       <div className="profiles">
-        {profiles.users.map((user, key) => (
-          <Link key={key} to={`/profiles/${user.id}`} className="link">
-            <Avatar
-              sx={{
-                bgcolor: lightBlue[500],
-                width: 100,
-                height: 100,
-                margin: 5,
-                "&:hover": { background: "blue" },
-              }}
-            >
-              {user.name}
-            </Avatar>
-          </Link>
-        ))}
+        {typeof profiles.users === "undefined" ? (
+          <p>Loading...</p>
+        ) : (
+          profiles.users.map((user, key) => (
+            <Link key={key} to={`/profiles/${user.id}`} className="link">
+              <Avatar
+                sx={{
+                  bgcolor: lightBlue[500],
+                  width: 100,
+                  height: 100,
+                  margin: 5,
+                  "&:hover": { background: "blue" },
+                }}
+              >
+                {user.name}
+              </Avatar>
+            </Link>
+          ))
+        )}
+        <Link to={"/profiles/create-profile"}>
+          <button className="add-profile">+</button>
+        </Link>
       </div>
     </div>
   );
